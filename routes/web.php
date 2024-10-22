944bfdb54ce5029134cc1355601d75be06999cf5
magdasaif <magdasaif3@gmail.com>
2024-10-22 23:42:14 +0300

    change show post route to be for guest

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', [HomeController::class,'index'])->name('home');
-
+//============================ authentication routes =========================================================
 Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+//================================== guest routes ============================================================
+Route::get('/home', [HomeController::class,'index'])->name('home');
+Route::get('/posts/{post}', [PostController::class,'show'])->name('posts.show');
+//======================================== auth routes =======================================================
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('/posts', PostController::class);
+    Route::resource('/posts', PostController::class)->except('show');
 });
-
+//=============================================================================================================
